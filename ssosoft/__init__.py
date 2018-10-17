fda63a180d9eee383aff60e90be50a78ce3623c1
SSOCsoft <gordonm@nmsu.edu>
2018-10-17 15:02:30 -0600

    Cleaned up code.

--- ssosoft/__init__.py
+++ ssosoft/__init__.py
@@ -1,13 +1,3 @@
 from ssosoft.RosaHardCamCal import *
 from . import ssosoftConfig
 
-#__author__="""Gordon A. MacDonald"""
-#__email__='gordonm@nmsu.edu'
-#__version__='0.1.0'
-#
-#idlPath="idl"
-#instrList=["IBIS", "FIRS", "SPINOR",
-#       "ROSA", "HARDCAM"]
-#ssocSoftRoot="/home/solarstorm/gordonm/dst_pro/"
-#synopticMode=""
-

e8dd09043669543d50f9064b3c9c2ae8b896ea69
SSOCsoft <gordonm@nmsu.edu>
2018-10-17 08:35:54 -0600

    Initial. SSOsoft configuration variables.

---
+++ ssosoft/ssosoftConfig.py
@@ -0,0 +1,12 @@
+"""Config file for SSOsoft"""
+
+__author__="""Gordon A. MacDonald"""
+__email__='gordonm@nmsu.edu'
+__version__='0.1.0'
+
+idlPath="idl"
+instrList=["IBIS", "FIRS", "SPINOR",
+       "ROSA", "HARDCAM"]
+ssocSoftRoot="/home/solarstorm/gordonm/dst_pro/"
+synopticMode=""
+

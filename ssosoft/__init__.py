aaea0867d90d7457ef797340eaf3b0a26f859623
SSOCsoft <gordonm@nmsu.edu>
2018-10-17 08:36:49 -0600

    Moved all configuration variables to ssosoftConfig.py.

--- ssosoft/__init__.py
+++ ssosoft/__init__.py
@@ -1,12 +1,13 @@
 from ssosoft.RosaHardCamCal import *
+from . import ssosoftConfig
 
-__author__="""Gordon A. MacDonald"""
-__email__='gordonm@nmsu.edu'
-__version__='0.1.0'
-
-idlPath="idl"
-instrList=["IBIS", "FIRS", "SPINOR",
-       "ROSA", "HARDCAM"]
-ssocSoftRoot="/home/solarstorm/gordonm/dst_pro/"
-synopticMode=""
+#__author__="""Gordon A. MacDonald"""
+#__email__='gordonm@nmsu.edu'
+#__version__='0.1.0'
+#
+#idlPath="idl"
+#instrList=["IBIS", "FIRS", "SPINOR",
+#       "ROSA", "HARDCAM"]
+#ssocSoftRoot="/home/solarstorm/gordonm/dst_pro/"
+#synopticMode=""
 

cb94a2611cfbc43eef51bfac0f735be09adfebfb
SSOCsoft <gordonm@nmsu.edu>
2019-02-13 13:47:03 -0700

    Added docstrings.

--- ssosoft/ssosoftConfig.py
+++ ssosoft/ssosoftConfig.py
@@ -1,14 +1,12 @@
 """
-Config file for SSOsoft
+Information and general parameters for the SSOsoft package.
 """
 
 __author__="""Gordon A. MacDonald"""
-__email__='gordonm@nmsu.edu'
+__email__="gordonm@nmsu.edu"
+__maintainer__="Gordon A. MacDonald"
 __version__='1.0.0'
 
-idlPath="idl"
 instrList=["IBIS", "FIRS", "SPINOR",
        "ROSA", "ZYLA"]
-ssocSoftRoot="/home/solarstorm/gordonm/dst_pro/"
-synopticMode=""
 

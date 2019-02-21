8d02034b6c4cdc87d05101df3f9d3502ba33605c
SSOCsoft <gordonm@nmsu.edu>
2019-02-21 12:31:36 -0700

    Version 1.1.0

--- ssosoft/ssosoftConfig.py
+++ ssosoft/ssosoftConfig.py
@@ -5,7 +5,7 @@ Information and general parameters for the SSOsoft package.
 __author__="""Gordon A. MacDonald"""
 __email__="gordonm@nmsu.edu"
 __maintainer__="Gordon A. MacDonald"
-__version__='1.0.0'
+__version__='1.1.0'
 
 instrList=["IBIS", "FIRS", "SPINOR",
        "ROSA", "ZYLA"]

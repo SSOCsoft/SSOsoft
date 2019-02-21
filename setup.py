37421a17d5fd971d9e8a779e515d13cf59e1012a
SSOCsoft <gordonm@nmsu.edu>
2019-02-21 12:55:13 -0700

    Version 1.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='SSOsoft',
-		version='1.1.0',
+		version='1.1.1',
 		description='Sunspot Solar Observatory Python Utilities',
 		author='Gordon A. MacDonald',
 		author_email='gordonm@nmsu.edu',
